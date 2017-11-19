946baf85dd17b3f6fbc2efea8804e7f3a245acb5
Autumn Fjeld <autumnfjeld@gmail.com>
2017-11-20 01:23:16 +1100

    Add test data

---
+++ src/__tests__/test-data.js
@@ -0,0 +1,37 @@
+/**
+ * Test Data for development and tests
+ */
+
+const testData = {
+  "results": [
+    {
+      "id": "1",
+      "name": "Iron Man",
+      "description": "Wounded, captured and forced to build a weapon by his enemies, billionaire industrialist Tony Stark instead created an advanced suit of armor to save his life and escape captivity. Now with a new outlook on life, Tony uses his money and intelligence to make the world a safer, better place as Iron Man.",
+      "thumbnail": {
+        "path": "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55",
+        "extension": "jpg"
+      }     
+    },
+    {
+      "id": "2",
+      "name": "Invisible Woman (Marvel: Avengers Alliance)",
+      "description": "Dear Marvel, women deserve descriptions as well. Suggestion: she that you can't see will destroy you.",
+      "thumbnail": {
+        "path": "http://i.annihil.us/u/prod/marvel/i/mg/6/80/5239b5c4cb782",
+        "extension": "jpg"
+      }     
+    },    
+   {
+      "id": "3",
+      "name": "Indestructable)",
+      "description": "Even though her title does not say 'woman' or 'girl' or indicate a female gender, she is indeed indestructable.",
+      "thumbnail": {
+        "path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4Kz6ragakh6XRgoSUWW5BJshql340x2p1B4TIUABfjhOWn82DZg",
+        "extension": "jpg"
+      }     
+    },      
+  ]
+}
+
+export default testData;
