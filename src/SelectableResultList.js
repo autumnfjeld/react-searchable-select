1de314d644eec04a48ed6236ff2df5a8b5391d8e
Autumn Fjeld <autumnfjeld@gmail.com>
2017-11-20 14:07:07 +1100

    Add SelectListItem component: a selectable button for each character

--- src/SelectableResultList.js
+++ src/SelectableResultList.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
+
+
 /**
- * A stateless component
+ * A stateless component for a single character
+ * @param {Object} props object passed from SelectableResultList
+ * TODO:  add character description under name
+ */
+function SelectListItem(props) {
+
+    const c = props.character;
+    const imgUrl = c.thumbnail.path + '.' + c.thumbnail.extension;
+
+    function characterSelected(name) {
+        console.log('characterSelected:', name);
+    }
+
+    return  <button className='btn-select' onClick={(e) => characterSelected(c.name)}>
+                <div className='marvel-img-box'>
+                    <img 
+                        className='marvel-img' 
+                        alt="TODOplaceholder"
+                        src={imgUrl}
+                    />
+                </div>
+                <div>
+                     <div className='marvel-name'> {c.name} </div>
+                </div>
+            </button>
+}
+
+/**
+ * A stateless component for list of characters
  * @param {Object} props - props object passed from declaration in SearchSelectContainer
  */
 function SelectableResultList(props) {
     if (props.characterList.length > 0) {
         return  <div className='result-list'>
-                    <ul>
-                        {props.characterList.map((c, id) => (
-                            <li key={id}>{c.name}</li>
-                        ))}
-                    </ul>
+                    {props.characterList.map((c, id) => (
+                        <SelectListItem key={id} character={c} />
+                    ))}
                 </div>
     } else {
         return null;
